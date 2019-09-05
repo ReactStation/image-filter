0b854b1833082943d4c84e2233b324610fa689f2
unknown <basel.almustafa@gmail.com>
2019-09-05 10:01:32 +0300

    Add endpoint to filter an image from a public url

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //   the filtered image file
+  app.get("/filteredimage", async (req, res) => {
+    //    1. validate the image_url query
+    let url = req.query.image_url;
+    if (!url) res.status(400).send("Missing image url!");
 
-  /**************************************************************************** */
+    //    2. call filterImageFromURL(image_url) to filter the image
+    let image: string;
+    try {
+      image = await filterImageFromURL(url);
+    } catch (error) {
+      console.log("ERROR ************************");
+      if (error) res.status(500).send("Failed to read image file!");
+    }
 
-  //! END @TODO1
+    //    3. send the resulting file in the response
+    //    4. deletes any files on the server on finish of the response
+    res.status(200).sendFile(image, err => {
+      if (err) res.status(500).send("Failed to send file!");
+      else deleteLocalFiles([...image]);
+    });
+  });
 
   // Root Endpoint
   // Displays a simple message to the user
